249387f827c543e1958424a363d66067f3a9764e
Chike Ozulumba <o.devcode@gmail.com>
2023-10-19 11:02:09 +0100

    chore: added loading spin

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -37,6 +37,7 @@ export const TextBox = () => {
     evt.preventDefault();
     if (!user) return;
     setProcessing(true);
+    // return;
 
     try {
       const formData = new FormData();
@@ -84,15 +85,38 @@ export const TextBox = () => {
               type="submit"
               className="inline-flex gap-2 items-center py-2.5 px-4 text-xs font-normal text-center text-white bg-blue-700 rounded-3xl focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-4 h-4 -rotate-90"
-              >
-                <path d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
-              </svg>
-              Share your testimony
+              {processing ? (
+                <svg
+                  className="animate-spin -ml-1 h-4 w-4 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    stroke-width="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="-ml-1 w-4 h-4 -rotate-90"
+                >
+                  <path d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
+                </svg>
+              )}
+              {processing ? "Saving" : "Share your testimony"}
             </button>
             <div className="flex pl-0 space-x-1 sm:pl-2">
               <Tooltip clickable id="attach-documents-button" />
