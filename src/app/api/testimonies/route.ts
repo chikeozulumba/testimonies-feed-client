c4734e23186e959a5f872eb53cfecce2a18d1083
Chike Ozulumba <o.devcode@gmail.com>
2023-10-19 10:29:08 +0100

    chore: fix bug - upload

--- src/app/api/testimonies/route.ts
+++ src/app/api/testimonies/route.ts
@@ -11,6 +11,7 @@ export async function POST(request: NextRequest) {
   const image = payload.get("image") as unknown as File;
   const attachment = payload.get("attachment") as unknown as File;
   const documentId = uuidv4();
+  const time = new Date().getTime();
 
   const files = [];
   if (!uid) return;
@@ -20,6 +21,7 @@ export async function POST(request: NextRequest) {
       name: image.name,
       file: image,
       type: "images",
+      key: "image",
     });
   }
 
@@ -28,15 +30,28 @@ export async function POST(request: NextRequest) {
       name: attachment.name,
       file: attachment,
       type: "attachments",
+      key: "attachment",
     });
   }
 
   const form = new FormData();
   files.forEach(({ file, type }) => form.append(type, file));
 
-  await sendFilesToWorker(form, String(uid), documentId);
+  await sendFilesToWorker(form, String(uid), documentId, time);
+  const record: Record<string, string | null | File> = {
+    comment,
+    uid,
+    image: null,
+    attachment: null,
+  };
 
-  await writeToFirestoreDB("testimonies", { comment, uid }, documentId);
+  files.forEach((file) => {
+    const fullUrlPath = `${file.type}/${uid}/${documentId}/${time}--${file.name}`;
+    const field = file.key as "image" | "attachment";
+    record[field] = fullUrlPath;
+  });
+
+  await writeToFirestoreDB("testimonies", record, documentId);
 
   return new NextResponse(
     JSON.stringify({
@@ -52,7 +67,8 @@ export async function POST(request: NextRequest) {
 async function sendFilesToWorker(
   form: FormData,
   uid: string,
-  documentId: string
+  documentId: string,
+  time: number
 ) {
   let result: any = null;
   let error: any = null;
@@ -61,13 +77,16 @@ async function sendFilesToWorker(
     const value = process.env.WORKER_AUTH_SECRET_KEY;
     const url = String(APP_CONFIG.WORKER_API_URL);
 
-    result = await axios.put(`${url}/?uid=${uid}&documentId=${documentId}`, form, {
-      headers: {
-        // 'Content-type': 'multipart/form-data',
-        [header]: value,
-      } as any,
-    });
-    console.log(result)
+    result = await axios.put(
+      `${url}/?uid=${uid}&documentId=${documentId}&time=${time}`,
+      form,
+      {
+        headers: {
+          [header]: value,
+        } as any,
+      }
+    );
+    console.log(result);
   } catch (e) {
     console.log(e);
     error = e;
